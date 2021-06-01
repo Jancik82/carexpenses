ec98db10f4d8a69e59cb513228bc3ef4ef04ff1b
Jancik82 <JanSlo7126>
2021-06-01 21:20:35 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/ce/carexpenses/CarexpensesApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.ce.carexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarexpensesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarexpensesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/ce/carexpenses/CarexpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.ce.carexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarexpensesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
